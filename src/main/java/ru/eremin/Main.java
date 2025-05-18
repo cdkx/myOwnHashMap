74deb0b29334ff3b2e9946ca9ce4b1c1bd0fcda1
Viktor Eremin <vektor451@gmail.com>
2025-05-18 15:17:15 +0300

    prototype v0.1

--- src/main/java/ru/eremin/Main.java
+++ src/main/java/ru/eremin/Main.java
@@ -1,12 +1,29 @@
 package ru.eremin;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        MyOwnHashMap map = new MyOwnHashMap();
-        map.put(1,"vasya");
+        MyOwnHashMap<String, String> map = new MyOwnHashMap<>();
 
-        System.out.println(map.get(1));
+        var key1 = "Hello1";
+        var key2 = "Hello2";
+        var key3 = "Hello3";
+        var key4 = "Hello4";
+
+
+        map.put(key1, "vasya");
+        map.put(key2, "petya");
+        map.put(key3, "grisha");
+        map.put(key4, "serega");
+
+
+        System.out.println(map.get(key1));
+        System.out.println(map.get(key2));
+        System.out.println(map.get(key3));
+        System.out.println(map.get(key4));
+
+        map.remove(key3);
+        System.out.println(map.get(key3));
+
     }
 }
--- src/main/java/ru/eremin/MyOwnHashMap.java
+++ src/main/java/ru/eremin/MyOwnHashMap.java
@@ -1,24 +1,53 @@
 package ru.eremin;
 
-import java.util.ArrayList;
-import java.util.List;
+
+public class MyOwnHashMap<K, V> {
+    private final Object[] array = new Object[16];
 
 
-public class MyOwnHashMap {
-    private final String[] array = new String[16];
+    public void put(K key, V value) {
+        Key<K> keyClass = new MyOwnHashMap.Key<>(key);
+        var index = (array.length - 1) & keyClass.hash();
 
-    public String get(Integer key) {
-        return array[key];
+        if (index >= array.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        array[index] = value;
+    }
+
+    public V get(K key) {
+        Key<K> keyClass = new MyOwnHashMap.Key<>(key);
+        var index = (array.length - 1) & keyClass.hash();
+        if (index >= array.length) {
+            return null;
+        }
+        @SuppressWarnings("unchecked")
+        V value = (V) array[index];
+        return value;
+    }
+
+    public void remove(K key) {
+        Key<K> keyClass = new MyOwnHashMap.Key<>(key);
+        var index = (array.length - 1) & keyClass.hash();
+        array[index] = null;
     }
 
 
-    public void put(Integer key, String value) {
-        array[key] = value;
-    }
+    public static class Key<K> {
+        private final K key;
 
+        public Key(K key) {
+            this.key = key;
+        }
 
-    public void remove(Integer key) {
-        array[key] = null;
+        public int hash() {
+            return key.hashCode();
+        }
 
+        @Override
+        public String toString() {
+            return key.toString();
+        }
     }
 }
