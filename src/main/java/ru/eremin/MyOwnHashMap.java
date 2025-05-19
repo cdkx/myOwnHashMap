41fb9de3783ce4fcbd363c048dd9f2a236b7030e
Viktor <vektor451@gmail.com>
2025-05-19 16:31:56 +0300

    TODO: comments and Improvements

--- src/main/java/ru/eremin/MyOwnHashMap.java
+++ src/main/java/ru/eremin/MyOwnHashMap.java
@@ -5,6 +5,16 @@ public class MyOwnHashMap<K, V> {
     private Node<K, V>[] nodes;
     private final float loadFactor;
 
+    // TODO
+    //  Замечания:
+    //  1) перепроверь метод put, по идее nodes[index].value = newNode.value будет перетирать 1 ноду, скорее всего нужно storedNode.value
+    //  2) словишь npe, если будешь передавать null ключ на key.hashCode()
+    //  3) checkResize выполняет за O(N), добавь поле size и упрости метод
+    //  Улучшения:
+    //  1) Добавить поддержку других методов интерфейса Map (итерация по entry Set, contains и тд)
+
+
+
     @SuppressWarnings("unchecked")
     public MyOwnHashMap() {
         this.nodes = (Node<K, V>[]) new Node[16];
